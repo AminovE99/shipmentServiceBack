8588f90f6269b7035e951ac63923073410c38f3c
AminovE99 <32329685+AminovE99@users.noreply.github.com>
2022-02-27 21:44:33 +0300

    :tada: Init app

---
+++ shipmentsService/shipmentsService/settings_test.py
@@ -0,0 +1,1 @@
+from shipmentsService.settings import *
---
+++ shipmentsService/shipmentsService/urls.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, re_path, include
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+class CustomSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super(CustomSchemaGenerator, self).get_schema(request, public)
+        schema.host = self.get_host(request)
+        schema.schemes = ["http", "https"]
+        return schema
+
+    @staticmethod
+    def get_host(request):
+        return request.META.get("HTTP_ORIGIN") or request.get_host()
+
+
+schema_view = get_schema_view(
+    openapi.Info(title="shipmentsService", default_version="v0.1"),
+    generator_class=CustomSchemaGenerator,
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(settings.API_PREFIX, include('shipments.urls')),
+    re_path(
+        r"swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
