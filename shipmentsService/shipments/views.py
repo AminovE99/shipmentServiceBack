f4758799856b733801d0c7fa108e846b52a6f1eb
Эмиль Аминов <amionv99@gmail.com>
2022-02-28 01:12:50 +0300

    :lipstick: beauty code with get_object_or_404

--- shipmentsService/shipments/views.py
+++ shipmentsService/shipments/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, mixins
-from rest_framework.generics import get_object_or_404
 
 from shipments.models import Shipment
 from shipments.serializers import ShipmentSerializer, ShipmentRetrieveSerializer
@@ -11,7 +10,3 @@ class ShipmentViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins
 
     def get_serializer_class(self, *args, **kwargs):
         return ShipmentRetrieveSerializer if self.action in ['list', 'retrieve'] else ShipmentSerializer
-
-    def get_object(self):
-        pk = self.kwargs.get('pk')
-        return get_object_or_404(Shipment, pk=pk)
